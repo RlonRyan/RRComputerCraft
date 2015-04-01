c8b9a3592fe1193a0361776573a1473f8f5060bf
RlonRyan <RlonRyan@users.noreply.github.com>
2015-04-01 13:45:03 -0600

    Update RRMine.lua
    Attempt to upgrade spatial awareness.

--- RRMine.lua
+++ RRMine.lua
@@ -1,3 +1,6 @@
+--
+-- Option Variables
+--
 mode = 0
 length = 0
 rows = 0
@@ -32,17 +35,17 @@ end
 function fuel()
 
 	while turtle.getFuelLevel()  == 0 do
-
+		
 		turtle.select(1)
 		turtle.refuel(1)
-
+		
 		if turtle.getFuelLevel() == 0 then
-
+			
 			term.write("Ran out of fuel! Refuel and press any key to continue!")
 			io.read()
-
+			
 		end
-
+		
 	end
 
 end
@@ -50,117 +53,163 @@ end
 function row(l)
 
 	for i=0,l,1 do
-
+		
 		fuel()
-
-		while not turtle.forward() do
-
+		
+		while turtle.detect() do
+			
 			turtle.dig()
-
+			
 		end
-
+		
+		move()
+		
 		if not turtle.detectDown() then
-
+			
 			turtle.select(3)
 			turtle.placeDown()
-
+			
 		end
-
+		
 		if i % torch == 1 then
-
+			
 			turtle.turnLeft()
 			turtle.turnLeft()
 			turtle.select(2)
 			turtle.place()
 			turtle.turnRight()
 			turtle.turnRight()
-
+			
 		end
-
+		
 		while turtle.detectUp() do
-
+			
 			turtle.digUp()
-
+			
 		end
-
+		
 	end
 end
 
 function turn()
 
 	if mode == 1 then
-
+		
 		turtle.turnRight()
-
+		dir = dir + 1
+		
+		if dir > 3 then
+			dir = 0
+		end
+		
 	else
-
+		
 		turtle.turnLeft()
-
+		dir = dir - 1
+		
+		if dir < 0 then
+			dir = 3
+		end
+		
 	end
 
 end
 
 function turnback()
 
-	if mode == -1 then
-
-		turtle.turnRight()
-
-	else
-
+	if mode == 1 then
+		
 		turtle.turnLeft()
-
+		dir = dir - 1
+		
+		if dir < 0 then
+			dir = 3
+		end
+		
+	else
+		
+		turtle.turnRight()
+		dir = dir + 1
+		
+		if dir > 3 then
+			dir = 0
+		end
+		
 	end
 
+end
+
+function face(d)
+	
+	while not dir == d do
+		turn()
+	end
+	
+end
+
+function move()
+	
+	if turtle.forward() then
+		
+		if dir == 0 then
+			y = y + 1
+		elseif dir == 1 then
+			x = x + 1
+		elseif dir == 2 then
+			y = y - 1
+		elseif dir == 3 then
+			x = x - 1
+		end
+		
+	end
+	
 end
 
 function depositinventory()
 
 	for i=4,9,1 do
-
+		
 		turtle.select(i)
 		turtle.drop()
-
+		
 	end		
 
 end
 
 function main()
-
+	
 	init()
-
+	
 	for i=0,rows,1 do
-
-		row(length)
-		turn()
-		row(2)
-		turn()
-		row(length)
-		turn()
-		row(2)
-
-		pos = 2
 		
-		while not turtle.detect() do
-
-			turtle.forward()
-			pos = pos + 1
-
+		row(length)
+		turn()
+		row(3)
+		turn()
+		row(length)
+		turn()
+		row(2)
+		pos = x
+		
+		while not x == 0 do
+			
+			move()
+			
 		end
-
+		
+		face(2)
 		depositinventory()
-		turn()
-		turn()
-
-		for ii=0,pos,1 do
-
-			turtle.forward()
-
+		face(1)
+		
+		while not x == pos do
+			
+			move()
+			
 		end
-
+		
 		row(2)
 		turnback()
-
+		
 	end
 
 end
