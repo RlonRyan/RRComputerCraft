7d8da8745442a70d39b487ab6bb5bdd7a503fb47
RlonRyan <RlonRyan@users.noreply.github.com>
2015-04-13 16:40:32 -0600

    Update RRWriteLine.lua

--- RRWriteLine.lua
+++ RRWriteLine.lua
@@ -3,21 +3,14 @@
 
 args = {...}
 
-function validate(filepath, overwrite)
-  --print(string.match(filepath, "%a+"))
+function validate(filepath)
   if(string.find(filepath, "/") and not fs.exists(string.match(filepath, "%a+"))) then
     fs.makeDir(string.match(filepath, "%a+"))
     return true
   elseif(not fs.exists(filepath)) then
     return true
-  elseif(not checkversion) then
-    fs.delete(filepath)
-    return true
-  elseif(overwrite) then
-    fs.delete(filepath)
-    return true
   else
-    return false
+    return true
   end
 end
 
@@ -27,12 +20,13 @@ for i = 3,table.getn(args) do
   line = line .. " " .. args[i]
 end
 
-if(validate(args[1], args[2] == "a")) then
+if(validate(args[1])) then
     
   temp = fs.open(args[1], args[2])
     
   if(not temp) then
     print("Cannot write file: " .. filepath)
+    temp.close()
     return false
   end
     
