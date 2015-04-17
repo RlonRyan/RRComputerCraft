7c771be436793686b20713f562b5c081d8b59808
RlonRyan <RlonRyan@users.noreply.github.com>
2015-04-17 13:45:27 -0600

    Update RRDownloader.lua
    Cleaned up GitHub downloads.

--- RRDownloader.lua
+++ RRDownloader.lua
@@ -1,69 +1,17 @@
--- Computer Program Agent
+-- Computer Program Downloader Agent
 -- By RlonRyan
 --
--- UgW5W9FM
+-- PpKH1HS8
 --
--- This program allows for the easy setup of RR|Programs (for ComputerCraft).
--- It will also attempt to update RR|Programs when run.
--- It uses the RR|Programs list found on github to find programs.
---
--- Dependencies:
--- (All dependencies will auto-download and install)
---
--- Installation instructions:
--- Use: pastebin get UgW5W9FM RR|Computer
---
--- Install disk instructions:
--- Save the line:
--- shell.run("pastebin get UgW5W9FM RR|Computer")
--- To disk/autorun
---
--- Run instructions:
--- Use: RR|Computer
+-- This program allows for the easy download of RR|Programs (for computercraft).
 
-local path = shell.getRunningProgram() .. "Resources"
+args = {...}
 
 local overwrite = true
 
-function output(text, color)
-  color = color or colors.white
-  term.setTextColor(color)
-  write(text)
-  term.setTextColor(colors.white)
-end
-
-function outputln(text, color) -- Dirty Funtion.
-  color = color or colors.white
-  term.setTextColor(color)
-  print(text)
-  term.setTextColor(colors.white)
-end
-
-function bar()
-	outputln("================", colors.lightGray)
-end
-
-function br()
-	print()
-end
-
-function err(...)
-  -- TODO: Add Error formatter.
-  br()
-  bar()
-  for line in arg do
-    outputln(text, colors.red)
-  end
-  bar()
-  br()
-end
-
-function reset()
-    term.clear()
-    term.setCursorPos(1,1)
-    outputln("RR|Program Setup", colors.green)
-    bar()
-    br()
+function checkVersion(filepath)
+  -- Todo
+  return false
 end
 
 function validate(filepath)
@@ -80,7 +28,19 @@ function validate(filepath)
     fs.delete(filepath)
     return true
   else
-  	err("Bad filepath: " .. filepath)
+    return false
+  end
+end
+
+function getManifest(address)
+  temp = http.get(address .. "/manifest.mf")
+  if(temp) then
+    manifest = textutils.unserialize(temp.readAll())
+    temp.close()
+    return manifest
+  else
+    print("Unable to fetch file. Is the internet down?")
+    print("Or have you not yet enabled http for computercraft?")
     return false
   end
 end
@@ -95,106 +55,51 @@ end
 
 function downloadHttp(address, filepath)
   if(not validate(filepath)) then
-  	err("Could not download: " .. address .. " to: " .. filepath)
+  	print("Could not download: " .. address .. " to: " .. filepath)
     return false
   else
-    outputln("Installing:", colors.gray)
-    outputln("\tFrom: " .. address, colors.gray)
-    outputln("\tTo: " .. filepath, colors.gray)
     temp = http.get(address)
     if(temp) then
       content = temp.readAll()
       temp.close()
       temp = fs.open(filepath, "w")
       if(not temp) then
-      	err("Cannot write file: " .. filepath)
+      	print("Cannot write file: " .. filepath)
       	return false
       end
       temp.write(content)
       temp.close()
       return true
     else
-      err("Unable to fetch file. Is the internet down?", "Or have you not yet enabled http for computercraft?")
+      print("Unable to fetch file. Is the internet down?")
+      print("Or have you not yet enabled http for computercraft?")
       return false
     end
   end
 end
 
-function getManifest(address)
-  temp = http.get(address .. "/manifest.mf")
-  if(temp) then
-    manifest = textutils.unserialize(temp.readAll())
-    temp.close()
-    return manifest
-  else
-    err("Unable to fetch file. Is the internet down?", "Or have you not yet enabled http for computercraft?")
-    return false
+function downloadGit(user, repo, branch, file, name)
+  address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch
+  print(address)
+  for k,v in pairs(getManifest(address)) do
+    downloadHttp(address .. "/" .. v["file"], v["program"])
   end
 end
 
-reset()
-
-print("Validating Directories.")
-fs.delete(path)
-fs.makeDir(path)
-
-reset()
-
-print("Updating programs list.")
-programs = getManifest("https://raw.githubusercontent.com/RlonRyan/RRComputerCraft/master")
-
-reset()
-
-outputln("Updating installation agent.")
-br()
-downloadHttp("https://raw.githubusercontent.com/RlonRyan/RRComputerCraft/master/RRDownloader.lua", path .. "/RRDownloader")
-
-reset()
-
-outputln("Obtaining Dependencies.")
-
-reset()
-
-outputln("Initialization Completed.")
-br()
-bar()
-print("")
-output("Press any key to continue.")
-io.read()
-
-reset()
-
-outputln("Available Programs")
-bar()
-menu = {}
-i = 1
-for k,v in pairs(programs) do
-	menu[i] = k
-	output(i .. ".) ", colors.yellow)
-	outputln(k .. " by " .. v["author"])
-	i = i + 1
-end
-br()
-bar()
-outputln("Select Programs: ")
-bar()
-
-selections = string.gmatch(io.read(), "[^%s]+")
-
-bar()
-
-for selection in selections do
-	
-	selection = tonumber(selection)
-	
-	if programs[menu[selection]]["source"] == "paste" then
-		shell.run(path .. "/RRDownloader", "paste", programs[menu[selection]]["paste"], programs[menu[selection]]["program"])
-	if programs[menu[selection]]["source"] == "http" then
-		shell.run(path .. "/RRDownloader", "http", programs[menu[selection]]["adress"], programs[menu[selection]]["program"])
-	elseif programs[menu[selection]]["source"] == "git" then
-		shell.run(path .. "/RRDownloader", "git", programs[menu[selection]]["author"], programs[menu[selection]]["repo"], programs[menu[selection]]["branch"], programs[menu[selection]]["file"], programs[menu[selection]]["program"])
-	end
+function downloadGitFull(user, repo, branch)
+  address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch
+  print(address)
+  for k,v in pairs(getManifest(address)) do
+    downloadHttp(address .. "/" .. v["file"], v["program"])
+  end
 end
 
-term.clear()
-term.setCursorPos(1,1)
+if(args[1] == "http" and table.getn(args) == 3) then
+	downloadPaste(args[2], args[3])
+elseif(args[1] == "http" and table.getn(args) == 3) then
+	downloadHttp(args[2], args[3])
+elseif(args[1] == "git" and table.getn(args) >= 6) then
+	downloadGit(args[2], args[3], args[4], args[5], args[6])
+else
+	print("Invalid mode.")
+end
