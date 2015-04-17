71c66b0a28fa8580f17e716def6478a97378e7e8
RlonRyan <RlonRyan@users.noreply.github.com>
2015-04-17 14:12:13 -0600

    Update RRDownloader.lua
    Added Dialogue.

--- RRDownloader.lua
+++ RRDownloader.lua
@@ -9,6 +9,55 @@ args = {...}
 
 local overwrite = true
 
+--
+-- Common Output Functions
+--
+
+function output(text, color)
+  color = color or colors.white
+  term.setTextColor(color)
+  write(text)
+  term.setTextColor(colors.white)
+end
+
+function outputln(text, color) -- Dirty Funtion.
+  color = color or colors.white
+  term.setTextColor(color)
+  print(text)
+  term.setTextColor(colors.white)
+end
+
+function bar()
+	outputln("================", colors.lightGray)
+end
+
+function br()
+	print()
+end
+
+function err(...)
+  -- TODO: Add Error formatter.
+  br()
+  bar()
+  for line in arg do
+    outputln(text, colors.red)
+  end
+  bar()
+  br()
+end
+
+function reset()
+    term.clear()
+    term.setCursorPos(1,1)
+    outputln("RR|Downloader", colors.green)
+    bar()
+    br()
+end
+
+--
+-- Main Functions
+--
+
 function checkVersion(filepath)
   -- Todo
   return false
@@ -46,55 +95,101 @@ function getManifest(address)
 end
 
 function downloadPaste(paste, filepath)
+	
   if(not validate(filepath)) then
+  	err("Invalid Filepath: " .. filepath)
     return false
   else
     return shell.run("pastebin", "get", paste, filepath)
   end
+  
 end
 
 function downloadHttp(address, filepath)
   if(not validate(filepath)) then
-  	print("Could not download: " .. address .. " to: " .. filepath)
+  	err("Invalid Filepath: " .. filepath)
     return false
   else
+  	
+  	br()
+  	outputln("Downloading")
+  	bar()
+  	
+  	output("Retrieving Temp. File... ")
     temp = http.get(address)
-    if(temp) then
-      content = temp.readAll()
-      temp.close()
-      temp = fs.open(filepath, "w")
-      if(not temp) then
-      	print("Cannot write file: " .. filepath)
-      	return false
-      end
-      temp.write(content)
-      temp.close()
-      return true
-    else
-      print("Unable to fetch file. Is the internet down?")
-      print("Or have you not yet enabled http for computercraft?")
-      return false
+    
+    if (not temp) then
+    	err("Unable to fetch file. Is the internet down? \n Or have you not yet enabled http for computercraft?")
+    	return false
+	end
+	
+    outputln("Retrieved.")
+    
+    br()
+    output("Reading File... ")
+    content = temp.readAll()
+    temp.close()
+    outputln("Read.")
+    
+    br()
+    output("Writing File... ")
+    temp = fs.open(filepath, "w")
+    if(not temp) then
+    	err("Cannot write file: " .. filepath)
+    	return false
     end
+    temp.write(content)
+    temp.close()
+    outputln(" Complete!")
+    
+    return true
+
   end
 end
 
 function downloadGit(user, repo, branch, file, name)
-  address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch
-  print(address)
-  for k,v in pairs(getManifest(address)) do
-    downloadHttp(address .. "/" .. v["file"], v["program"])
-  end
+	
+	output("Compiling GitHub Address: ")
+  	address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch .. "/" .. file
+  	outputln(address, colors.gray)
+  	bar()
+  	
+  	downloadHttp(address, name)
+  	
 end
 
 function downloadGitFull(user, repo, branch)
-  address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch
-  print(address)
-  for k,v in pairs(getManifest(address)) do
-    downloadHttp(address .. "/" .. v["file"], v["program"])
-  end
+	
+	output("Compiling GitHub Address: ")
+  	address = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch
+  	outputln(address, colors.gray)
+  	
+  	for k,v in pairs(getManifest(address)) do
+    	downloadHttp(address .. "/" .. v["file"], v["program"])
+  	end
+  	
 end
 
-if(args[1] == "http" and table.getn(args) == 3) then
+--
+--	Main Code
+--
+
+reset()
+
+if (table.getn(args) < 3) then
+	
+	err("Not enough arguments provided to download.")
+	return false
+	
+end
+
+outputln("Downloading Program: " .. args[table.getn(args)])
+bar()
+output("\tFrom: " .. args[2])
+output("\tVia: " .. args[1])
+bar()
+
+if(args[1] == "paste" and table.getn(args) == 3) then
 	downloadPaste(args[2], args[3])
 elseif(args[1] == "http" and table.getn(args) == 3) then
 	downloadHttp(args[2], args[3])
@@ -103,3 +198,6 @@ elseif(args[1] == "git" and table.getn(args) >= 6) then
 else
 	print("Invalid mode.")
 end
+
+reset()
+outputln("Done.", colors.lime)
